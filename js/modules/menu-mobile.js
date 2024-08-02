eafea73a58dc57ab613b9267822e356f041d42df
Paulo Chagas <paulofilhochagas@gmail.com>
2024-08-01 22:30:28 -0300

    correcao geral de bugs

--- js/modules/menu-mobile.js
+++ js/modules/menu-mobile.js
@@ -17,7 +17,8 @@ export default class initMenuMobile {
   // abre o menu mobile ao clicar nele
   // e fecha ao clicar fora
 
-  openMenu() {
+  openMenu(event) {
+    event.preventDefault();
     this.menuList.classList.add(this.activeClass);
     this.menuButton.classList.add(this.activeClass);
     outsideClick(this.menuList, this.events, () => {
